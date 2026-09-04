04bc835898cb2c95474c4d9bc7488250fd92bc03
shihab-sani <mdsani633@gmail.com>
2026-09-04 16:00:07 +0600

    Initialize Enterprise-Multi-Tenant-SaaS-POS-Application project with Spring Boot structure, Maven configuration, and IDE settings.

---
+++ Enterprise-Multi-Tenant-SaaS-POS-Application/src/main/java/posapplication/multitenantsaas/EnterpriseMultiTenantSaaSPosApplication.java
@@ -0,0 +1,13 @@
+package posapplication.multitenantsaas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnterpriseMultiTenantSaaSPosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnterpriseMultiTenantSaaSPosApplication.class, args);
+    }
+
+}
---
+++ Enterprise-Multi-Tenant-SaaS-POS-Application/src/test/java/posapplication/multitenantsaas/EnterpriseMultiTenantSaaSPosApplicationTests.java
@@ -0,0 +1,13 @@
+package posapplication.multitenantsaas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnterpriseMultiTenantSaaSPosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
