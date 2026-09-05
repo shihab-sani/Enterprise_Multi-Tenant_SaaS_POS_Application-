23702def6e0ece1295e984c6505956ad006c0a86
shihab-sani <mdsani633@gmail.com>
2026-09-05 22:07:56 +0600

    Add SecurityConfig with JWT validation, CORS setup, and role-based access control

---
+++ Enterprise-Multi-Tenant-SaaS-POS-Application/src/main/java/posapplication/multitenantsaas/Config/SecurityConfig.java
@@ -0,0 +1,46 @@
+package posapplication.multitenantsaas.Config;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.Collections;
+import java.util.List;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+
+        return httpSecurity.sessionManagement(management ->
+                management.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(Authorize ->
+                        Authorize.requestMatchers("/api/**").authenticated()
+                                .requestMatchers("/api/super-admin/**").hasRole("ADMIN").anyRequest().permitAll())
+                .addFilterBefore(new JwtValidator(), BasicAuthenticationFilter.class)
+                .csrf(AbstractHttpConfigurer::disable).cors(cors ->
+                        cors.configurationSource(corsConfigurationSource())).build();
+    }
+
+    private CorsConfigurationSource corsConfigurationSource() {
+        return new CorsConfigurationSource() {
+            @Override
+            public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+                CorsConfiguration corsConfiguration = new CorsConfiguration();
+                corsConfiguration.setAllowedOrigins(List.of("http://localhost:4000"));
+                corsConfiguration.setAllowCredentials(true);
+                corsConfiguration.setAllowedHeaders(Collections.singletonList("*"));
+                corsConfiguration.setExposedHeaders(List.of("Authorization"));
+                corsConfiguration.setMaxAge(3600L);
+                return corsConfiguration;
+            }
+        };
+    }
+}
