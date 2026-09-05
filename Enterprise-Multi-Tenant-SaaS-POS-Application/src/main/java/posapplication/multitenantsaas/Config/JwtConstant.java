2bba8de01724bd75c3a4fefdccfdd854279e58ed
shihab-sani <mdsani633@gmail.com>
2026-09-05 22:08:06 +0600

    Implement JWT authentication by adding JwtProvider, JwtValidator, and JwtConstant classes for token generation, validation, and configuration.

---
+++ Enterprise-Multi-Tenant-SaaS-POS-Application/src/main/java/posapplication/multitenantsaas/Config/JwtConstant.java
@@ -0,0 +1,6 @@
+package posapplication.multitenantsaas.Config;
+
+public class JwtConstant {
+    public static final String JWT_SECRET = "2c339cfa89dc952225ed0123e404f63dd9cf1c9a7d4b65a5c0291027931ee8f3";
+    public static final String JWT_HEADER = "Authorization";
+}
---
+++ Enterprise-Multi-Tenant-SaaS-POS-Application/src/main/java/posapplication/multitenantsaas/Config/JwtProvider.java
@@ -0,0 +1,48 @@
+package posapplication.multitenantsaas.Config;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.util.Collection;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+public class JwtProvider {
+    static SecretKey key = Keys.hmacShaKeyFor(JwtConstant.JWT_SECRET.getBytes());
+
+    public String generateToken(Authentication authentication) {
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+
+        String role = populateAuthorities(authorities);
+
+        return Jwts.builder()
+                .issuedAt(new Date())
+                .expiration(new Date(new Date().getTime()+86400000))
+                .claim("email", authentication.getName())
+                .claim("authorities", role)
+                .signWith(key)
+                .compact();
+    }
+
+    public String getEmailFromToken(String token) {
+        token = token.substring(7);
+        Claims claims = Jwts.parser().verifyWith(key).build().parseSignedClaims(token).getPayload();
+
+        return String.valueOf(claims.get("email"));
+    }
+
+    private String populateAuthorities(Collection<? extends GrantedAuthority> authorities) {
+        Set<String> auth = new HashSet<>();
+        for (GrantedAuthority authority : authorities) {
+            auth.add(authority.getAuthority());
+        }
+        return String.join(",", auth);
+    }
+}
