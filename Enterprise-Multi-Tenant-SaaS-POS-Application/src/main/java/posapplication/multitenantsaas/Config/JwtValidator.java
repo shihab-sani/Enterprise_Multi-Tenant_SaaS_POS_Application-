e1d3eea4ce975eb252b0d4206c5690fcd4a82bbf
shihab-sani <mdsani633@gmail.com>
2026-09-10 17:00:47 +0600

    Fix misplaced `filterChain.doFilter` call in `JwtValidator` to ensure proper request filtering

--- Enterprise-Multi-Tenant-SaaS-POS-Application/src/main/java/posapplication/multitenantsaas/Config/JwtValidator.java
+++ Enterprise-Multi-Tenant-SaaS-POS-Application/src/main/java/posapplication/multitenantsaas/Config/JwtValidator.java
@@ -43,7 +43,7 @@ public class JwtValidator extends OncePerRequestFilter {
             } catch (Exception e) {
                 throw new BadCredentialsException("Invalid JWT");
             }
-            filterChain.doFilter(request,response);
         }
+        filterChain.doFilter(request,response);
     }
 }
